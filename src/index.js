41e4af1294336d6f763281d95fa957e71676854d
Inna Ponomareva <innaponomareva@MBP-von-Inna.fritz.box>
2022-11-16 04:34:19 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {createStore} from 'redux';
-import AppReducer from './store/AppReducer';
-import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
-import 'material-design-icons/iconfont/material-icons.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import AppReducer from "./store/AppReducer";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import "material-design-icons/iconfont/material-icons.css";
 
 const store = createStore(AppReducer);
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
-      <App/>
+      <App />
     </Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </HashRouter>,
+  document.getElementById("root")
 );
